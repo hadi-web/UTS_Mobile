57ccd4de4dd99f774ada553159b944c67e059dda
hadi-cyber <hadikusumanto2000@gmail.com>
2020-11-13 18:05:31 +0700

    UTS

---
+++ Hadi Kusumanto_18090112_5C/app/src/main/java/com/example/uts/TentangActivity.kt
@@ -0,0 +1,13 @@
+package com.example.uts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.uts.R
+
+class TentangActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tentang)
+
+    }
+}
