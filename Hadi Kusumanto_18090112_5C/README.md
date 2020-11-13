b137df7f6e549445b7b1b2b7a2bf9e46a9630b65
hadi-cyber <hadikusumanto2000@gmail.com>
2020-11-13 18:03:07 +0700

    .

--- Hadi Kusumanto_18090112_5C/README.md
+++
@@ -1,4 +0,0 @@
-# UTS_android
-anggota : 
-1. Ade Sugiantoro 18090064 5B
-2. Titis Eka S 18090049 5B
--- Hadi Kusumanto_18090112_5C/app/src/main/java/com/example/uts/helper/Constant.kt
+++
@@ -1,9 +0,0 @@
-package com.example.uts.helper
-
-class Constant {
-    companion object {
-        val PREF_IS_LOGIN = "PREF_IS_LOGIN"
-        val PREF_USERNAME = "PREF_USERNAME"
-        val PREF_PASSWORD = "PREF_PASSWORD"
-    }
-}
